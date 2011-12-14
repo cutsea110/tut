5cfb032d52f30643933392def64c0782e7ffa267
Katsutoshi Itoh <cutsea110@gmail.com>
2011-12-15 05:17:32 +0900

    Update user profile using IForm

--- Handler/Root.hs
+++ Handler/Root.hs
@@ -3,6 +3,7 @@
 module Handler.Root where
 
 import Import
+import Data.Text (pack)
 
 -- This is a handler function for the GET request method on the RootR
 -- resource pattern. All of your resource patterns are defined in
@@ -26,10 +27,22 @@ getHomeR uid = do
     return (me, fs)
   let sexs = [minBound..maxBound]
       sexIs s = s == userSex self
+      toInt = (+1).fromEnum
   defaultLayout $ do
     setTitle "user home"
     $(widgetFile "userhome")
 
 postHomeR :: UserId -> Handler RepHtml
 postHomeR uid = do
+  (sex,age,memo) <- runInputPost $ (,,)
+                    <$> ireq (selectField sexs) "sex"
+                    <*> ireq intField "age"
+                    <*> iopt textField "memo"
+  runDB $ do
+    update uid [ UserSex =. sex
+               , UserAge =. age
+               , UserMemo =. maybe "" id memo
+               ]
   redirect RedirectTemporary $ HomeR uid
+  where
+    sexs = [(pack $ show s, s) | s <- [minBound..maxBound]]
