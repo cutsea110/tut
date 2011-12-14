63b3e5e0f1d75bce28064e84524cd2a7a9cfa88d
Katsutoshi Itoh <cutsea110@gmail.com>
2011-12-14 23:06:20 +0900

    Listing up friends by using database

--- Handler/Root.hs
+++ Handler/Root.hs
@@ -3,7 +3,6 @@
 module Handler.Root where
 
 import Import
-import Data.Text as T
 
 -- This is a handler function for the GET request method on the RootR
 -- resource pattern. All of your resource patterns are defined in
@@ -21,7 +20,7 @@ getRootR = do
 
 getHomeR :: Text -> Handler RepHtml
 getHomeR name = do
-  let friends = T.words "Kate Michael James"
+  friends <- runDB $ selectList [] []
   defaultLayout $ do
     setTitle "user home"
     $(widgetFile "userhome")
